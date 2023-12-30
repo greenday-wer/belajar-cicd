71cc3c5be7b4569d7e840840b75a6314209a584f
greenday-wer <hartawank3@gmail.com>
2023-12-31 02:52:27 +0800

    ubah

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
   res.end('Hello, World!\n');
 });
 
-const port = 3000;
+
 server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:2020`);
 });
