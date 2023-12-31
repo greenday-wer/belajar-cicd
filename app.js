9eee5fc81ad376293b916a780e3d351c66aa100d
greenday-wer <hartawank3@gmail.com>
2023-12-31 12:56:02 +0800

    ubah

--- app.js
+++ app.js
@@ -2,8 +2,9 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, Worldm0!\n');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
 });
 
 
